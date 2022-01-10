557fc82fc4049e797188ee776e741a56adfa7257
josephlara <joseph.lara@gmail.com>
2022-01-10 13:28:47 +0200

    update to mq

--- Scripts/em_mq.R
+++ Scripts/em_mq.R
@@ -13,30 +13,244 @@ library(glue)
 # DEFINE REPORTING MONTH AND FILE PATHS -------------------------------------------
 
 month <- "2021-12-20" # UPDATE EVERY MONTH
+monthly_dataset <- ("Data/Ajuda/ER_DSD_TPT_VL/VL/_CompileHistoric/VL_2021_12.csv") # PATH AND NAME OF MONTHLY DATASET BEING PROCESSED AND SAVED TO DISK
 
 ajuda_path <- "~/GitHub/AJUDA_Site_Map/Dataout/AJUDA_Site_Map_20211206.xlsx"
-df_path <- "Data/Ajuda/ER_DSD_TPT_VL/2021_12/NON MER Indicators Template_FY22 12_20_2021a.xlsx"
+echo <- "Data/Ajuda/ER_DSD_TPT_VL/2021_12/Monitoria Intensiva_ Template_FY22 12_20_2021_updated_ECHO.xlsx"
+fgh <- "Data/Ajuda/ER_DSD_TPT_VL/2021_12/FGH_DEC_21_Monitoria Intensiva Template FY22_122021_Updated_ January 6_2022.xlsx"
 
 # LOAD DATASETS -----------------------------------------------------------
 
-df <- read_excel({df_path}, 
-                   sheet = "Monitoria Intensiva de CV", 
-                   skip = 9)
-
-ajuda <- read_excel(ajuda_path) %>% 
+ajuda_site_map <- read_excel(ajuda_path) %>% 
   select(orgunituid,
          SNU,
          Psnu, 
-         Sitename) %>% 
+         Sitename,
+         partner = `IP FY20`) %>% 
   select_all(str_to_lower)
 
 # CREATE FUNCTION TO TIDY CV DATASETS -------------------------------------
 
-cv_reshape <- function(filename, ip){
+cv_tidy <- function(filename, ip){
   
-  df <- read_excel({df_path}, 
-                   sheet = "Monitoria Intensiva de CV", 
-                   skip = 9) %>%
+  df <- read_excel(filename, 
+                   sheet = "Monitoria Intensiva", 
+                   skip = 9,
+                   col_types = c("text",
+                                 "text",
+                                 "text",
+                                 "text",
+                                 "text",
+                                 "text",
+                                 "text",
+                                 "text",
+                                 "text",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric")
+                   ) %>%
   pivot_longer('dpi.colheu.pcr_d_total':'mds.cv.supressao_prop_mds', 
                names_to = c("indicator", "numdenom", "pop_type", "age"), 
                names_sep = "_", 
@@ -66,13 +280,48 @@ cv_reshape <- function(filename, ip){
   
 }
 
-# FUNCTION TEST -----------------------------------------------------------
+# IMPORT & RESHAPE cv SUBMISSIONS -----------------------------------------------------------
 
-df_test <- cv_reshape(df, "ARIEL")
+echo <- cv_tidy(echo, "ECHO")
+fgh <- cv_tidy(fgh, "FGH")
 
-# PRINT TO TO DISK ----------------------------------------------------------
+# COMPILE IP SUMBISSIONS --------------------------------------------------
 
-readr::write_tsv(
-  df_test,
-  "Dataout/mi_cv_test.txt")
+cv_tidy <- dplyr::bind_rows(echo, fgh)
+
+cv_tidy %>% # TABLE HF SUBMISSION LINES BY PARTNER.  CAUTION - BLANK SUBMISSION LINES ARE INCLUDED!
+  group_by(Partner) %>% 
+  distinct(DATIM_code) %>% 
+  summarise(n())
+
+rm(echo, fgh)
+
+# WRITE MONTHLY TPT CSV TO DISK ------------------------------------
+
+readr::write_csv(
+  cv_tidy,
+  {monthly_dataset})
+
+# TEMPORARY WORKAROUND ----------------------------------------------------
+
+cv_compile <- read_csv("Data/Ajuda/ER_DSD_TPT_VL/VL/_CompileHistoric/manual_compile/VL_2021_12_12.csv") %>% 
+  select(-c(No,
+            Type,
+            Data,
+            Partner,
+            Province,
+            District, 
+            `Health Facility`)) %>% 
+  left_join(ajuda_site_map, by = c("DATIM_code" = "orgunituid")) %>% 
+  rename(orgunituid = DATIM_code,
+         sisma_id = SISMA_code,
+         province = snu,
+         district = psnu,
+         site = sitename) %>% 
+  relocate(month:partner, .after = sisma_id) %>% 
+  glimpse()
+
+write_tsv(
+  cv_compile,
+  "Dataout/em_cv.txt")
 
