642c56e300d28e0ae5325d5013890f4494de2cf7
josephlara <joseph.lara@gmail.com>
2022-05-18 22:34:05 +0200

    Update erdsd_transform_mizela.R

--- Scripts/erdsd_transform_mizela.R
+++ Scripts/erdsd_transform_mizela.R
@@ -16,3 +16,42 @@ library(lubridate)
 
 
 df <- read_csv("Data/Ajuda/ERDSD/old/AJUDA_transformed_Mar22.txt")
+
+df_1 <- df %>% 
+  select(DATIM_code,
+         period = Months,
+         numdenom = NumDen,
+         age = AgeAsEntered,
+         sex = Sex,
+         dsd_eligibility = DSD_Eligibility, # NEED TO INCLUDE
+         pop_type = PatientType,
+         key_pop = KeyPop,
+         er_status = ER_Status, # NEED TO INCLUDE
+         dispensation = Dispensation,
+         indicator = Indicator,
+         value)
+
+
+df_2 <- df_1 %>% 
+  filter(!pop_type == "Total") %>%
+  mutate(indicator = recode(indicator, 
+                            "MMD" = "TX_MMD",
+                            "previous_TX_CURR" = "TX_CURR_Previous",
+                            "DSD" = "DSD_D",
+                            "OneDSD" = "DSD_D",
+                            "3MDD" = "DSD_3MD",
+                            "6MDD" = "DSD_6MD",
+                            "FluxoRa" = "DSD_FR",
+                            "GAAC" = "DSD_GAAC",
+                            "AFam" = "DSD_AF",
+                            "ClubA" = "DSD_CA",
+                            "DComm" = "DSD_DC",
+                            "ER1Month" = "ER_1",
+                            "ER4Month" = "ER_4"))
+
+
+
+
+distinct(df_2, indicator)
+
+  
