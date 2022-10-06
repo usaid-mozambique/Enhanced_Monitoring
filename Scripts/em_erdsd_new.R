6e8bc6faa1806f7e0bb62a5ed177b6f857e639fa
josephlara <joseph.lara@gmail.com>
2022-10-06 17:37:56 +0200

    Work to refine new data structure

--- Scripts/em_erdsd_new.R
+++ Scripts/em_erdsd_new.R
@@ -1,42 +1,56 @@
-
-# LOAD CORE TIDYVERSE & OTHER PACKAGES ------------------------------------
-
-
 rm(list = ls())
+
+# DEPENDENCIES ------------------------------------------------------------
+
 
 library(tidyverse)
 library(glamr)
+library(googlesheets4)
+library(googledrive)
+library(fs)
+library(lubridate)
 library(janitor)
 library(readxl)
 library(openxlsx)
 library(glue)
-library(ggthemes)
-library(scales)
-library(lubridate)
+library(gt)
+load_secrets() 
 
 
-# DEFINE MONTH AND PATHS ---------------------------
+# VALUES & PATHS ---------------------------
+
+# update each month
+month <- "20/09/2022" 
+file <- "IMER_2022_09"
+
+# update each month
+
+TEST <- "Data/Ajuda/ER_DSD_TPT_VL/2022_09/MonthlyEnhancedMonitoringTemplates_FY22_Oct 5 2022 Submission_Tete.xlsx"
+
+# DOD <- "Data/Ajuda/ER_DSD_TPT_VL/2022_08/MonthlyEnhancedMonitoringTemplates_FY22_August2022_DOD.xlsx"
+# ARIEL <- "Data/Ajuda/ER_DSD_TPT_VL/2022_09/MonthlyEnhancedMonitoringTemplates_FY22_Oct 5 2022 Submission_Tete.xlsx"
+# CCS <- "Data/Ajuda/ER_DSD_TPT_VL/2022_08/MonthlyEnhancedMonitoringTemplates_FY22_August2022 CCS.xlsx"
+# ECHO <- "Data/Ajuda/ER_DSD_TPT_VL/2022_08/MonthlyEnhancedMonitoringTemplates_FY22_August2022_ECHO.xlsx"
+# EGPAF <- "Data/Ajuda/ER_DSD_TPT_VL/2022_08/MonthlyEnhancedMonitoringTemplates_FY22_August2022 EGPAF.xlsx"
+# ICAP <- "Data/Ajuda/ER_DSD_TPT_VL/2022_08/MonthlyEnhancedMonitoringTemplates_FY22_August2022_ICAP.xlsx"
+# FGH <- "Data/Ajuda/ER_DSD_TPT_VL/2022_08/MonthlyEnhancedMonitoringTemplates_FY22_August2022_FGH.xlsx"
 
 
-month <- "20/04/2022" # UPDATE EACH MONTH
-monthly_dataset <- ("Dataout/ERDSD/_CompileHistoric/ERDSD_2022_04.txt") # UPDATE EACH MONTH
-
-DOD <- "Data/Ajuda/ER_DSD_TPT_VL/2022_04/DOD__April_2022final 20042022 DOD Jhpiego New Template.xlsx"
-ARIEL <- "Data/Ajuda/ER_DSD_TPT_VL/2022_04/ARIEL Monitoria Intensiva and DSD Template_FY22_April 2022.xlsx"
-CCS <- "Data/Ajuda/ER_DSD_TPT_VL/2022_04/CCS_Monitoria Intensiva_ Template_FY22Abril.xlsx"
-ECHO <- "Data/Ajuda/ER_DSD_TPT_VL/2022_04/Monitoria Intensiva_ Template_FY22Q2_APR_ECHO_V2.xlsx"
-EGPAF <- "Data/Ajuda/ER_DSD_TPT_VL/2022_04/EGPAF_Monitoria Intensiva_ Template_FY22Q2 Abril_2022.xlsx"
-ICAP <- "Data/Ajuda/ER_DSD_TPT_VL/2022_04/ICAP_Abril_2022_Monitoria Intensiva_ Template_FY22Q3_updated11052022.xlsx"
-FGH <- "Data/Ajuda/ER_DSD_TPT_VL/2022_04/Monitoria Intensiva_ Template_FY22Q2_FGH_Montlhy_data_April_05052022.xlsx"
 
 
-historic_files_path <- "Dataout/ERDSD/_CompileHistoric/" # DOES NOT REQUIRE UPDATING EACH MONTH
+# do not update each month
+path_ajuda_site_map <- as_sheets_id("1CG-NiTdWkKidxZBDypXpcVWK2Es4kiHZLws0lFTQd8U") # path for fetching ajuda site map in google sheets
+path_monthly_output_repo <- "Dataout/IMER/monthly_processed/" # folder path where monthly dataset archived
+path_monthly_output_file <- path(path_monthly_output_repo, file, ext = "txt") # composite path/filename where monthly dataset saved
+path_monthly_output_gdrive <- as_id("https://drive.google.com/drive/folders/12bkLnrQNXbKpbyo-zwk9dmxS6NHDyLwU") # google drive folder where monthly dataset saved 
+path_historic_output_file <- "Dataout/em_imer.txt" # folder path where monthly dataset archived
+path_historic_output_gdrive <- as_id("https://drive.google.com/drive/folders/1xBcPZNAeYGahYj_cXN5aG2-_WSDLi6rQ") # google drive folder where historic dataset saved
 
+# METADATA -----------------------------------------------------------
 
-# LOAD METADATA -----------------------------------------------------------
+erdsd_var_mapping <- read_excel("Documents/erdsd_var_mapping.xlsx", sheet = "Sheet5")
 
-
-ajuda_site_map <- read_excel("~/GitHub/AJUDA_Site_Map/Dataout/AJUDA Site Map.xlsx") %>%
+ajuda_site_map <- read_sheet(path_ajuda_site_map) %>%
   select(sisma_uid = sisma_id,
          datim_uid =  orgunituid,
          site_nid,
@@ -56,20 +70,17 @@ ajuda_site_map <- read_excel("~/GitHub/AJUDA_Site_Map/Dataout/AJUDA Site Map.xls
          longitude = Long)
 
 
-# CREATE FUNCTION TO RESHAPE SUBMISSIONS ---------------------------------------------
+# FUNCTIONS ---------------------------------------------
 
 
-erdsd_var_mapping <- read_excel("Documents/erdsd_var_mapping.xlsx", sheet = "Sheet2")
-
-
-erdsd_reshape <- function(filename, ip){
+imer_reshape <- function(filename, ip){
   
   df <- read_excel(filename, 
-                   sheet = "ER_DSD", 
+                   sheet = "TX NEW, TX CURR AND IMER", 
                    skip = 8,
                    col_types = "text") %>% 
     select(!c(No, SISMA_code, Period)) %>% 
-    pivot_longer(TX_NEWTot:DCommTotNoEl10_14, 
+    pivot_longer(TX_NEWTot:I4_ER4_40_RetCalc, 
                  names_to = "indicator", 
                  values_to = "value") %>% 
     inner_join(erdsd_var_mapping, by = "indicator") %>% 
@@ -85,8 +96,8 @@ erdsd_reshape <- function(filename, ip){
            age = recode(age,
                         "unknown" = "Unknown"),
            sex = recode(sex,
-                  "M" = "Male",
-                  "F" = "Female")) %>% 
+                        "M" = "Male",
+                        "F" = "Female")) %>% 
     filter(Partner == ip) %>% 
     select(partner = Partner,
            snu = Province,
@@ -117,113 +128,154 @@ erdsd_reshape <- function(filename, ip){
 }
 
 
-# IMPORT & RESHAPE SUBMISSIONS -------------------------------------------------
+
+df <- read_excel(TEST, 
+                 sheet = "TX NEW, TX CURR AND IMER", 
+                 skip = 8,
+                 col_types = "text") %>% 
+  select(!c(No, SISMA_code, Period)) %>% 
+  pivot_longer(TX_NEWTot:I4_ER4_40_RetCalc, 
+               names_to = "indicator", 
+               values_to = "value") %>% 
+  inner_join(erdsd_var_mapping, by = "indicator") %>% 
+  filter(!indicator_new == "remove") %>% 
+  separate(indicator_new, 
+           c("indicator", "sex", "age", "pop_type", "dispensation", "numdenom", "er_status", "dsd_eligibility"),
+           sep = "//.") %>% 
+  mutate(across(everything(), ~ifelse(.=="", NA, as.character(.))),
+         value = as.numeric(value),
+         period = as.Date(month, "%d/%m/%Y"),
+         indicator = str_replace_all(indicator, "\\.", "_"),
+         age = str_replace_all(age, "\\.", "-"),
+         age = recode(age,
+                      "unknown" = "Unknown"),
+         sex = recode(sex,
+                      "M" = "Male",
+                      "F" = "Female"),
+         key_pop = case_when(pop_type == "FSW" ~ "FSW",
+                             pop_type == "MSM" ~ "MSM",
+                             pop_type == "PWID" ~ "PWID",
+                             pop_type == "PPCS" ~ "PPCS"),
+         pop_type = recode(pop_type,
+                           "FSW" = "KP",
+                           "MSM" = "KP",
+                           "PWID" = "KP",
+                           "PPCS" = "KP"),
+         pop_type = case_when(age %in% c("<15", "<1", "1-4", "5-9", "10-14") ~ "Pediatric",
+                              age %in% c("15+", "15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "50+", "65+") ~ "Adult")) %>% 
+  filter(Partner == "ECHO") %>% 
+  select(partner = Partner,
+         snu = Province,
+         psnu = District,
+         sitename = `Health Facility`,
+         datim_uid = DATIM_code,
+         period,
+         indicator,
+         sex,
+         age,
+         pop_type,
+         key_pop,
+         dispensation,
+         numdenom,
+         er_status,
+         dsd_eligibility,
+         value)
+
+tx_curr_prev <- df %>%
+  filter(indicator == "TX_CURR") %>% 
+  mutate(indicator = recode(indicator,
+                            "TX_CURR" = "TX_CURR_Previous"),
+         period = period + months(1))
+
+df <- bind_rows(df, tx_curr_prev)
 
 
-ariel <- erdsd_reshape(ARIEL, "ARIEL")
-echo <- erdsd_reshape(ECHO, "ECHO")
-ccs <- erdsd_reshape(CCS, "CCS")
-egpaf <- erdsd_reshape(EGPAF, "EGPAF")
-fgh <- erdsd_reshape(FGH, "FGH")
-icap <- erdsd_reshape(ICAP, "ICAP")
-dod <- erdsd_reshape(DOD, "JHPIEGO-DoD")
+df %>% distinct(indicator, sex, age, pop_type) %>% print(n=150)
+df %>% distinct(indicator, key_pop, er_status) %>% print(n=150)
 
+test <- df %>% filter(is.na(pop_type)) %>% distinct(indicator, pop_type)
+test <- df %>% filter(indicator == "TX_NEW")
 
-# COMPILE SUMBISSIONS --------------------------------------------------
-
-
-er_dsd <- bind_rows(echo, dod, ariel, ccs, egpaf, fgh, icap)
-
-er_dsd <- bind_rows(echo)
-# rm(dod, ariel, ccs, echo, egpaf, fgh, icap)
-
-
-# IDENTIFY ERRORS COMMON IN MONTHLY SUBMISSION --------------------------------------------------
-
-
-check_missing_datim_uid <- er_dsd %>% 
-  anti_join(ajuda_site_map, by = "datim_uid") %>% 
-  distinct(partner, snu, psnu, sitename, datim_uid)
-
-
-totals <- er_dsd %>% 
-  count(indicator,
-        wt = value,
-        name = "total")
   
+# FUNCTIONS RUN -------------------------------------------------
+
+# test <- imer_reshape(TEST, "ECHO")
 
 
-# WRITE MONTHLY CSV TO DISK ------------------------------------
+# dod <- imer_reshape(DOD, "JHPIEGO-DoD")
+# echo <- imer_reshape(ECHO, "ECHO")
+# ariel <- imer_reshape(ARIEL, "ARIEL")
+# ccs <- imer_reshape(CCS, "CCS")
+# egpaf <- imer_reshape(EGPAF, "EGPAF")
+# fgh <- imer_reshape(FGH, "FGH")
+# icap <- imer_reshape(ICAP, "ICAP")
+
+glimpse(test)
+
+# NEED TO CREATE key_pop var and recode pop_type key pop to KP
+
+# COMPILE DATASETS --------------------------------------------------
 
 
-readr::write_tsv(
-  er_dsd,
-  na = "NA",
-  {monthly_dataset})
 
 
-#---- SURVEY ALL MONTHLY DATASETS THAT NEED TO BE COMBINED FOR HISTORIC DATASET ---------------------------------
+# MONTHLY FILE WRITE ------------------------------------
 
 
-historic_files <- dir({historic_files_path}, pattern = "*.txt")
-
-erdsd_tidy_history <- historic_files %>%
-  map(~ read_tsv(file.path(historic_files_path, .))) %>%
-  reduce(rbind) %>% 
-  subset(period < max(period))
 
 
-#---- ROW BIND ALL IP SUBMISSION AND GENERATE OUTPUT -----------------------
+# HISTORIC DATASET BUILD ---------------------------------
 
 
-volumn_period <- erdsd_tidy_history %>% 
-  select(datim_uid, period, indicator, value) %>%
-  filter(period == max(period),
-         indicator == "TX_CURR") %>% 
-  group_by(datim_uid, .drop = TRUE) %>% 
-  summarize(across(where(is.numeric), ~ sum(.x, na.rm = TRUE))) %>%
-  mutate(site_volume = case_when(
-    value < 1000 ~ "Low",
-    between(value, 1000, 5000) ~ "Medium",
-    value > 5000 ~ "High",
-    TRUE ~ "Not Reported")) %>% 
-  select(datim_uid, site_volume) %>% 
+
+
+
+# METADATA JOIN ---------------------------------
+
+imer_tidy_historic <- ariel %>% 
+  select(-c(partner,
+            snu,
+            psnu,
+            sitename)) %>%
+  left_join(ajuda_site_map, by = c("datim_uid" = "datim_uid")) %>%
   glimpse()
 
 
-#---- JOIN AJUDA SITEMAP & CLEAN DATAFRAME -----------------------
+# OUTPUT CLEAN -----------------------
 
 
-erdsd_tidy_history_2 <- erdsd_tidy_history %>%
-  left_join(ajuda_site_map, by = c("datim_uid" = "datim_uid")) %>% 
-  left_join(volumn_period) %>% 
+imer_tidy_historic_2 <- imer_tidy_historic %>%
   select(datim_uid,
          sisma_uid,
          site_nid,
          period,
-         partner = partner.y,
-         snu = snu.y,
-         psnu = psnu.y,
-         sitename = sitename.y,
-         site_volume,
+         partner,
+         snu,
+         psnu,
+         sitename,
          ends_with("tude"),
          starts_with("support"),
          starts_with("his"),
-         sex,
-         age,
+         indicator,
          pop_type,
          dispensation,
+         numdenom,
          er_status,
          dsd_eligibility,
-         numdenom,
-         indicator, 
+         sex,
+         age,
          value) %>% 
   glimpse()
 
 
-# PRINT FINAL OUTPUT TO DISK ----------------------------------------------
 
 
-readr::write_tsv(
-  erdsd_tidy_history_2,
-  "Dataout/em_erdsd_new.txt")
+# OUTPUT WRITE ----------------------------------------------
+
+
+
+# PLOTS & TABLES ---------------------------------------------------------------
+
+
+
+
