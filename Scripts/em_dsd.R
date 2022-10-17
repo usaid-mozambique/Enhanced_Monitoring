698b4c47211ebcfa6fda82b619a64d664a766f93
josephlara <joseph.lara@gmail.com>
2022-10-17 15:11:11 +0200

    finalizing dsd script

--- Scripts/em_dsd.R
+++ Scripts/em_dsd.R
@@ -41,9 +41,9 @@ FGH <- glue::glue("{path_monthly_input_repo}MonthlyEnhancedMonitoringTemplates_F
 
 # do not update each month
 path_ajuda_site_map <- as_sheets_id("1CG-NiTdWkKidxZBDypXpcVWK2Es4kiHZLws0lFTQd8U") # path for fetching ajuda site map in google sheets
-path_monthly_output_repo <- "Dataout/IMER/monthly_processed/" # folder path where monthly dataset archived
+path_monthly_output_repo <- "Dataout/DSD/monthly_processed/" # folder path where monthly dataset archived
 path_monthly_output_file <- path(path_monthly_output_repo, file, ext = "txt") # composite path/filename where monthly dataset saved
-path_monthly_output_gdrive <- as_id("https://drive.google.com/drive/folders/12bkLnrQNXbKpbyo-zwk9dmxS6NHDyLwU") # google drive folder where monthly dataset saved 
+path_monthly_output_gdrive <- as_id("https://drive.google.com/drive/folders/15x2biGIIYrY_eW-zrKQbrvMT47cI5yOE") # google drive folder where monthly dataset saved 
 path_historic_output_file <- "Dataout/em_dsd.txt" # folder path where monthly dataset archived
 path_historic_output_gdrive <- as_id("https://drive.google.com/drive/folders/1xBcPZNAeYGahYj_cXN5aG2-_WSDLi6rQ") # google drive folder where historic dataset saved
 
@@ -72,40 +72,142 @@ ajuda_site_map <- read_sheet(path_ajuda_site_map) %>%
 
 # FUNCTIONS ---------------------------------------------
 
-df <- read_excel(ECHO, 
-                 sheet = "MDS", 
-                 skip = 8) %>% 
-  select(!c(No, SISMA_code, Period)) %>% 
-  pivot_longer(remove.1:DSD.AHD__LW_15p, 
-               names_to = c("indicator", "dsd_eligibility", "pop_type", "age"),
-               names_sep = "_",
-               values_to = "value") %>% 
-  filter(!str_detect(indicator, "remove")) %>% 
-  mutate(indicator = str_replace_all(indicator, "\\.", "_"),
-         age = str_replace_all(age, "\\.", "-"),
-         age = case_when(age == "15p" ~ "15+",
-                         age == "2u" ~ "<2",
-                         TRUE ~ age),
-         dsd_eligibility = recode(dsd_eligibility,
-                                  ELI = "Eligible",
-                                  NEL = "Non-Eligible",
-                                  TOTAL = NA_character_),
-         # dsd_eligibility = case_when(
-         #   dsd_eligibility == "ELI" ~ "Eligible",
-         #   dsd_eligibility == "NEL" ~ "Non-Eligible",
-         #   age == "<2" & indicator %in% c("DSD_PCD", "DSD_MB", "DSD_MC", "DSD_FA", "DSD_AC", "DSD_HE", "DSD_1STB", "DSD_1S", "DSD_1SYF", "DSD_1SMCH", "DSD_AHD") ~ "Non-Eligible",
-         #   pop_type %in% c("PW", "LW") & indicator %in% c("DSD_PCD", "DSD_MB", "DSD_MC", "DSD_FA", "DSD_AC", "DSD_HE", "DSD_1STB", "DSD_1S", "DSD_1SYF", "DSD_1SMCH", "DSD_AHD") ~ "Non-Eligible",
-         #   NA ~ "Unknown"),
-         pop_type = recode(pop_type, 
-                           ADULT = "Adult",
-                           PED = "Pediatric")) %>% 
+
+dsd_reshape <- function(df, ip) {
+  
+  df <- read_excel(df, # function argument
+                   sheet = "MDS", 
+                   skip = 8) %>% 
+    select(!c(No, SISMA_code, Period)) %>% 
+    pivot_longer(remove.1:DSD.AHD__LW_15p, 
+                 names_to = c("indicator", "dsd_eligibility", "pop_type", "age"),
+                 names_sep = "_",
+                 values_to = "value") %>% 
+    filter(Partner == ip, # function argument
+           !str_detect(indicator, "remove")) %>% 
+    mutate(period = as.Date(month, "%Y-%m-%d"),
+           indicator = str_replace_all(indicator, "\\.", "_"),
+           age = str_replace_all(age, "\\.", "-"),
+           age = case_when(age == "15p" ~ "15+",
+                           age == "2u" ~ "<2",
+                           TRUE ~ age),
+           dsd_eligibility = recode(dsd_eligibility,
+                                    ELI = "Eligible",
+                                    NEL = "Non-Eligible",
+                                    TOTAL = NA_character_),
+           pop_type = recode(pop_type, 
+                             ADULT = "Adult",
+                             PED = "Pediatric")) %>% 
+    select(partner = Partner,
+           snu = Province,
+           psnu = District,
+           sitename = `Health Facility`,
+           datim_uid = DATIM_code,
+           period,
+           indicator,
+           dsd_eligibility,
+           pop_type,
+           age,
+           value)
+
+}
+
+
+# FUNCTIONS RUN -------------------------------------------------
+
+
+dod <- dsd_reshape(DOD, "JHPIEGO-DoD")
+echo <- dsd_reshape(ECHO, "ECHO")
+ariel <- dsd_reshape(ARIEL, "ARIEL")
+ccs <- dsd_reshape(CCS, "CCS")
+egpaf <- dsd_reshape(EGPAF, "EGPAF")
+fgh <- dsd_reshape(FGH, "FGH")
+icap <- dsd_reshape(ICAP, "ICAP")
+
+
+# COMPILE IP SUMBISSIONS --------------------------------------------------
+
+
+dsd <- bind_rows(dod, ariel, ccs, echo, egpaf, fgh, icap)
+rm(dod, ariel, ccs, echo, egpaf, fgh, icap)
+
+# detect lines not coded with datim_uids
+dsd %>% 
+  filter(is.na(datim_uid)) %>% 
+  distinct(datim_uid, snu, psnu, sitename) %>% 
+  anti_join(ajuda_site_map, by = c("datim_uid" = "datim_uid"))
+
+
+# WRITE MONTHLY TPT CSV TO DISK ------------------------------------
+
+
+# write to local
+readr::write_tsv(
+  dsd,
+  na = "",
+  {path_monthly_output_file})
+
+# write to google drive
+drive_put(path_monthly_output_file,
+          path = path_monthly_output_gdrive,
+          name = glue({file}, '.txt'))
+
+
+# SURVEY ALL MONTHLY DATASETS THAT NEED TO BE COMBINED FOR HISTORIC DATASET ---------------------------------
+
+
+historic_files <- dir({path_monthly_output_repo}, pattern = "*.txt")  # PATH FOR PURR TO FIND MONTHLY FILES TO COMPILE
+
+dsd_tidy_historic <- historic_files %>%
+  map(~ read_tsv(file.path(path_monthly_output_repo, .))) %>%
+  reduce(rbind) 
+
+
+
+# METADATA JOIN ---------------------------------
+
+dsd_tidy_historic_2 <- dsd_tidy_historic %>% 
+  filter(period <= as.Date(month)) %>% 
+  select(-c(partner,
+            snu,
+            psnu,
+            sitename)) %>%
+  left_join(ajuda_site_map, by = c("datim_uid" = "datim_uid")) %>%
   glimpse()
 
-# report headers need updating as a broad set of dsd models are not coded for eligibility
-df %>% distinct(indicator, dsd_eligibility) %>% print(n=100)
-sum(df$value, na.rm=T)
-temp <- df %>% filter(dsd_eligibility == "")
-unique(temp$indicator)
-ind <- unique(temp$indicator)
 
-temp <- df %>% filter(indicator == "TX_ACTIVE")
+
+# OUTPUT CLEAN -----------------------
+
+
+dsd_tidy_historic_3 <- dsd_tidy_historic_2 %>%
+  select(datim_uid,
+         sisma_uid,
+         site_nid,
+         period,
+         partner,
+         snu,
+         psnu,
+         sitename,
+         ends_with("tude"),
+         starts_with("support"),
+         starts_with("his"),
+         indicator,
+         pop_type,
+         dsd_eligibility,
+         age,
+         value) %>% 
+  glimpse()
+
+
+# OUTPUT WRITE ----------------------------------------------
+
+
+readr::write_tsv(
+  dsd_tidy_historic_3,
+  "Dataout/em_dsd.txt")
+
+# write to google drive
+drive_put(path_historic_output_file,
+          path = path_historic_output_gdrive)
+
