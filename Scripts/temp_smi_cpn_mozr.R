5c1f45f5acb7ebb2775b8409796515e03a987929
josephlara <joseph.lara@gmail.com>
2023-03-09 17:25:23 +0200

    Enhanced Monitoring: Complete smi_cpn parsing function

--- Scripts/temp_smi_cpn_mozr.R
+++ Scripts/temp_smi_cpn_mozr.R
@@ -21,7 +21,7 @@ load_secrets()
 # DEFINE PATHS ------------------------------------------------------------
 
 
-year <- "2023"
+year <- "2022"
 
 tarv_path <- glue::glue("Data/MISAU/SMI/sisma_cpn_{year}.csv")
 
@@ -32,54 +32,107 @@ tarv_path <- glue::glue("Data/MISAU/SMI/sisma_cpn_{year}.csv")
 
 smi_cpn <- clean_sisma_csv(tarv_path)
 
-indicator_list <- distinct(smi_cpn, indicator)
-
 
 df <- smi_cpn %>% 
-
+  
   dplyr::filter(!is.na(value)) %>% 
   
   dplyr::mutate(
     
-    indicator_temp = dplyr::case_when(stringr::str_detect(indicator, "_anos") ~ "MG_1CON_MES",
-                                      stringr::str_detect(indicator, "12_semanas") ~ "MG_1CON_12SEM_MES",
-                                      stringr::str_detect(indicator, "total_da_coorte") ~ "MG_1CON_COORTE",
-                                      stringr::str_detect(indicator, "_4_ou_mais_") ~ "MG_4CON_COORTE",
-                                      stringr::str_detect(indicator, "parceiros_presentes") ~ "PARCEIRO_PRESENTE",
-                                      stringr::str_detect(indicator, "parceiros_testado") ~ "PARCEIRO_TESTADO",
-                                      stringr::str_detect(indicator, "iniciaram_ctz|ctz_a_entrada") ~ "MG_HIV_CTZ",
-                                      stringr::str_detect(indicator, "positiva_a_entrada|gravidas_testadas_hiv") ~ "MG_ESTADO_HIV",
-                                      stringr::str_detect(indicator, "tarv_a_entrada|iniciaram_tarv|monoprofilaxia|biprofilaxia") ~ "MG_HIV_POS_ARV",
-                                      
-                                      stringr::str_detect(indicator, "2_doses_de_tip") ~ "MG_MAL_TIP_2DOS",
-                                      stringr::str_detect(indicator, "4_ou_mais_doses_de_tip") ~ "MG_MAL_TIP_4DOS",
-                                      stringr::str_detect(indicator, "remtil") ~ "MG_MAL_REMTIL",
-                                      stringr::str_detect(indicator, "diagnostico_laboratorial") ~ "MG_MAL_DIAG",
-                                      stringr::str_detect(indicator, "tratamento_para_malaria") ~ "MG_MAL_DIAG_TX"),
+    indicator = dplyr::case_when(
+      
+      # consultation feature engineering
+      stringr::str_detect(indicator, "_anos") ~ "MG_CON1_MES",
+      stringr::str_detect(indicator, "12_semanas") ~ "MG_CON1_12SEM_MES",
+      stringr::str_detect(indicator, "total_da_coorte") ~ "MG_CON1_COORTE",
+      stringr::str_detect(indicator, "4_ou_mais_consultas") ~ "MG_CON4_COORTE",
+      
+      # hiv feature engineering
+      stringr::str_detect(indicator, "parceiros_presentes") ~ "PARCEIRO_PRESENTE",
+      stringr::str_detect(indicator, "parceiros_testado") ~ "PARCEIRO_HIV_TESTADO",
+      stringr::str_detect(indicator, "iniciaram_ctz|ctz_a_entrada") ~ "MG_HIV_CTZ",
+      stringr::str_detect(indicator, "positiva_a_entrada|gravidas_testadas_hiv") ~ "MG_HIV_ESTADO",
+      stringr::str_detect(indicator, "tarv_a_entrada|iniciaram_tarv|monoprofilaxia|biprofilaxia") ~ "MG_HIV_POS_ARV",
+      
+      # nutrition feature engineering
+      stringr::str_detect(indicator, "ganho") ~ "MG_NUT_GANHO_2TRI",
+      stringr::str_detect(indicator, "aguda_grave") ~ "MG_NUT_DAG",
+      stringr::str_detect(indicator, "aguda_moderada") ~ "MG_NUT_DAM",
+      stringr::str_detect(indicator, "suplementos") ~ "MG_NUT_SUPLEM",
+      stringr::str_detect(indicator, "curadas") ~ "MG_NUT_DESNUT_CUR",
+      stringr::str_detect(indicator, "abandonaram") ~ "MG_NUT_DESNUT_ABND",
+      stringr::str_detect(indicator, "desparasitante") ~ "MG_NUT_DESPARA",
+      stringr::str_detect(indicator, "ferroso") ~ "MG_NUT_SALFER_3D",
+      
+      # malaria feature engineering
+      stringr::str_detect(indicator, "2_doses_de_tip") ~ "MG_MAL_TIP_2DOS",
+      stringr::str_detect(indicator, "4_ou_mais_doses") ~ "MG_MAL_TIP_4DOS",
+      stringr::str_detect(indicator, "remtil") ~ "MG_MAL_REMTIL",
+      stringr::str_detect(indicator, "diagnostico_laboratorial") ~ "MG_MAL_DIAG",
+      stringr::str_detect(indicator, "tratamento_para_malaria") ~ "MG_MAL_DIAG_TX",
+      
+      # its feature engineering
+      stringr::str_detect(indicator, "diagnostico_sindromico") ~ "MG_ITS_DIAG", # ok
+      stringr::str_detect(indicator, "tratamento_sindromico") ~ "MG_ITS_DIAG_TX", # ok
+      stringr::str_detect(indicator, "3a_dose_de_tratamento_de_sifilis") ~ "MG_ITS_SIF_3DOS", # ok
+      stringr::str_detect(indicator, "para_sifilis") ~ "MG_ITS_TESTADA_SIF", # 
+      stringr::str_detect(indicator, "receberam_tratamento_de_sifilis") ~ "PARCEIRO_SIF_TX",
+      
+      # tb feature engineering
+      stringr::str_detect(indicator, "inh") ~ "MG_TB_INH",
+      stringr::str_detect(indicator, "tuberculose") ~ "MG_TB_TX",
+      
+      # other feature engineering
+      stringr::str_detect(indicator, "misoprostol") ~ "MG_HPP_MISOPROSTOL",
+      stringr::str_detect(indicator, "1a_dose_vat") ~ "MG_VAT_1D",
+      stringr::str_detect(indicator, "5a_dose_vat") ~ "MG_VAT_2_5D"),
+    
     
     age = dplyr::case_when(stringr::str_detect(indicator, "10_14") ~ "10-14",
                            stringr::str_detect(indicator, "15_19") ~ "15-19",
                            stringr::str_detect(indicator, "20_24") ~ "20-24",
                            stringr::str_detect(indicator, "_25_") ~ "25+"),
     
-    disaggregate = dplyr::case_when(stringr::str_detect(indicator, "hiv_positiva_a_entrada") ~ "HIV+ a entrada",
-                                    stringr::str_detect(indicator, "testadas_hiv_positivas") ~ "HIV+ testado/a",
-                                    stringr::str_detect(indicator, "testadas_hiv_negativas") ~ "HIV- testado/a",
-                                    stringr::str_detect(indicator, "parceiros_testados_positivos") ~ "HIV+ testado/a",
-                                    stringr::str_detect(indicator, "parceiros_testados_negativos") ~ "HIV- testado/a",
-                                    stringr::str_detect(indicator, "iniciaram_ctz") ~ "Inicio CTZ",
-                                    stringr::str_detect(indicator, "ctz_a_entrada") ~ "CTZ a entrada",
-                                    stringr::str_detect(indicator, "tarv_a_entrada") ~ "TARV a entrada",
-                                    stringr::str_detect(indicator, "iniciaram_tarv") ~ "Inicio TARV",
-                                    stringr::str_detect(indicator, "monoprofilaxia") ~ "Monoprofilaxia",
-                                    stringr::str_detect(indicator, "iniciaram_tarv") ~ "biprofilaxia"),
     
-    period_cohort = dplyr::if_else(indicator_temp %in% c("MG_1CON_MES", "MG_1CON_12SEM_MES"), period, period - months(6)))
+    disaggregate = dplyr::case_when(
+      
+      # hiv testing feature engineering
+      stringr::str_detect(indicator, "hiv_positiva_a_entrada") ~ "Positivo a entrada",
+      stringr::str_detect(indicator, "testadas_hiv_positivas") ~ "Positivo testado/a",
+      stringr::str_detect(indicator, "testadas_hiv_negativas") ~ "Negativo testado/a",
+      stringr::str_detect(indicator, "parceiros_testados_positivos") ~ "Positivo testado/a",
+      stringr::str_detect(indicator, "parceiros_testados_negativos") ~ "Negativo testado/a",
+      
+      # pmtct arv feature engineering
+      stringr::str_detect(indicator, "tarv_a_entrada") ~ "TARV a entrada",
+      stringr::str_detect(indicator, "iniciaram_tarv") ~ "Inicio TARV",
+      stringr::str_detect(indicator, "monoprofilaxia") ~ "Monoprofilaxia",
+      stringr::str_detect(indicator, "iniciaram_tarv") ~ "biprofilaxia",
+
+      # its feature engineering
+      stringr::str_detect(indicator, "positivas_para_sifilis") ~ "Sifilis Positiva",
+      stringr::str_detect(indicator, "negativas_para_sifilis") ~ "Sifilis Negativa",
+            
+      # ctz feature engineering
+      stringr::str_detect(indicator, "iniciaram_ctz") ~ "Inicio CTZ",
+      stringr::str_detect(indicator, "ctz_a_entrada") ~ "CTZ a entrada"),
+      
     
+    # calculate aligned period for 1st anc indicators
+    period_cohort = dplyr::if_else(indicator %in% c("MG_1CON_MES", "MG_1CON_12SEM_MES"), period, period - months(6))
+    
+  ) %>% 
+  
+  dplyr::select(starts_with("period"), snu, psnu, sitename, sisma_uid, indicator, age, disaggregate, value)
 
 
 
-    
+glimpse(df)
+
+write_excel_csv(temp, file = "temporary.csv")
+
+temp <- as.tibble(unique(df$indicator_temp))
+
 df %>% 
   filter(indicator_temp == "MG_1CON_MES") %>% 
   ggplot(aes(period, value, fill = age)) +
@@ -90,85 +143,5 @@ df %>%
     y = "No. 1st ANC") +
   scale_y_continuous(labels = scales::percent_format(accuracy = 1L)) +
   facet_grid(~ snu) 
-
-
-
-  
-  
-  dplyr::relocate(period_cohort, .after = period) %>% 
-  
-  glimpse()
-
-
-
-
-parceiros_testados_negativos
-parceiros_testados_positivos
-
-
-unique(df$indicator_temp)
-    
-    sex = dplyr::case_when(stringr::str_detect(indicator, "feminino")             ~ "Feminino",
-                           stringr::str_detect(indicator, "masculino")            ~ "Masculino",
-                           stringr::str_detect(indicator, "_0_4_anos|_5_9_anos")  ~ "Desconh."),
-    
-    age = dplyr::case_when(stringr::str_detect(indicator, "0_4")       ~ "<05",
-                           stringr::str_detect(indicator, "5_9")     ~ "05-09",
-                           stringr::str_detect(indicator, "10_14")   ~ "10-14",
-                           stringr::str_detect(indicator, "15_19")   ~ "15-19",
-                           stringr::str_detect(indicator, "20+")       ~ "20+",
-                           stringr::str_detect(indicator, " 0_14")   ~ "<15",
-                           stringr::str_detect(indicator, "15+")       ~ "15+",
-                           TRUE ~ NA_character_),
-    
-    age_coarse = dplyr::case_when(age %in% c("<05", "05-09", "10-14", "<15") ~ "<15",
-                                  age %in% c("15-19", "20+", "15+")          ~ "15+",
-                                  TRUE ~ age),
-    
-    exit_type = dplyr::case_when(stringr::str_detect(indicator, "uspensos")         ~ "Suspenso",
-                                 stringr::str_detect(indicator, "bitos")            ~ "Obito",
-                                 stringr::str_detect(indicator, "bandonos")         ~ "Abandono",
-                                 stringr::str_detect(indicator, "ransferidos_para") ~ "Transferidos Para"),
-    
-    indicator2 = dplyr::case_when(stringr::str_detect(indicator, "iniciou TARV nesta unidade sanitaria durante") ~ "TX_NOVO",
-                                  stringr::str_detect(indicator, "iniciou TARV nesta unidade sanitaria ate") ~ "TX_NOVO_CUM"))
-    
-
-unique(df$sex)
-unique(df$age)
-unique(df$age_coarse)
-unique(df$exit_type)
-unique(df$sex)
-unique(df$sex)
-unique(df$sex)
-
-
-    indicator2 = dplyr::case_when(stringr::str_detect(indicator, "iniciou TARV nesta unidade sanitaria durante") ~ "TX_NOVO",
-                                  stringr::str_detect(indicator, "iniciou TARV nesta unidade sanitaria ate") ~ "TX_NOVO_CUM"))
-    
-
-
-unique(df$indicator2)
-
-    source = "RM HIV/SIDA",
-    
-  )
-
-
-df_sample <- df %>% 
-  filter(sisma_uid == "XmuwpVFIIzP")
-
-unique(df_sample$exit_type)
-
-
-
-view(df)
-
-
-
-
-x <- "MZ HIV SIDA - NÂº de pacientes que iniciou PrÃ©-TARV (cuidados HIV) nesta unidade sanitÃ¡ria durante o mÃªs - F 5 - 9 anos"
-
-stringi::stri_trans_general(x, "latin-ascii")
 
 
